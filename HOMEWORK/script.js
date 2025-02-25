772b44d1385fc2d50f50c8604d8bc8a86a5d0c33
tornikekhvadagiani <tornikexvadagiani1@gmail.com>
2025-02-25 14:21:48 +0400

    finish

--- HOMEWORK/script.js
+++ HOMEWORK/script.js
@@ -1,62 +1,38 @@
 // დავალება 1
 
 function Sum(a, b) {
-  if ((typeof a === "number") & (typeof b === "number")) {
-    return a + b;
-  } else {
-    return "შეცდომა მატრიცაში";
-  }
+  if (typeof a !== "number" || typeof b !== "number" || a!==b) return "შეცდომა მატრიცაში";
+  return "ტოლია"
 }
 
-let sum = Sum(6, 8);
-
-console.log(sum);
+console.log(Sum ("6", "6"));
+console.log(Sum (6, 6));
+console.log(Sum (6, "6"));
 
 // დავალება 2
 
 function ConvertFahrenheit(f) {
-  if (typeof f === "number") {
+  if (typeof f !== "number") return false;
     return (f - 32) / 1.8;
-  } else {
-    return false;
   }
-}
 
-let celsiusValue = ConvertFahrenheit(60);
-
-console.log(celsiusValue);
-
+console.log(ConvertFahrenheit("60"));
+console.log(ConvertFahrenheit(60));
 
 // დავალება 3
 
 function Calculator(x, y, operator) {
-  if (
-    operator != "/" &&
-    operator != "*" &&
-    operator != "-" &&
-    operator != "+"
-  ) {
-    return false;
-  }
-
-  if (typeof x != "number" || typeof y != "number") {
-    return false;
-  }
+  if (typeof x !== "number" || typeof y !== "number")return false;
 
   switch (operator) {
-    case "/":
-      return x / y;
-
-    case "*":
-      return x * y;
-
-    case "-":
-      return x - y;
-
-    case "+":
-      return x + y;
+    case "/":return x / y;
+    case "*":return x * y;
+    case "-":return x - y;
+    case "+":return x + y;
+    default:return false
   }
 }
 
-let result = Calculator(10, 2, "+");
-console.log(result);
+console.log(Calculator(10, 2, "+"));
+console.log(Calculator(10, 2, "^"));
+console.log(Calculator(10, "2", "+"));
